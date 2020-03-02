448d319d4d0809922a4f61cfa8bee295fdc0ca81
FerranLao <ferranlao@hotmail.com>
2020-03-02 20:14:54 +0100

    cors added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express      = require('express');
-const logger       = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
-const mongoose = require("mongoose")
-const passport = require("passport")
+const express = require("express");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const cors = require("cors");
 const app = express();
 
 mongoose
@@ -20,24 +21,33 @@ mongoose
     console.error("Error connecting to mongo", err);
   });
 
+app.locals.title = "Express - Generated with IronGenerator";
 
-app.locals.title = 'Express - Generated with IronGenerator';
+const whitelist = ["http://localhost:3001", "http://localhost:3002"];
 
-app.use(logger('dev'));
+const corsConfig = {
+  origin: (origin, cb) => {
+    cb(null, whitelist.includes(origin));
+  },
+  credentials: true
+};
+app.use(cors(corsConfig));
+
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/auth",require("./routes/auth"))
+app.use("/auth", require("./routes/auth"));
 
 require("./passport/authenticateToken");
 app.use(passport.initialize());
 app.use(passport.authenticate("jwt", { session: false }));
 
-app.use("/books",require("./routes/books"))
+app.use("/books", require("./routes/books"));
 
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -46,11 +56,11 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
